a0c7ad23345e383a35f3a3683c9bac2687fecaa1
Ryan Blue <rdblue@gmail.com>
2010-01-21 20:20:59 +0100

    Added grit, marker, and svn/core to environment.

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem 'grit'
+  config.gem 'marker'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
@@ -39,3 +41,8 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
+# be sure to install libsvn-ruby to get the ruby SVN bindings on ubuntu
+require 'svn/core'
+require 'grit'
+require 'marker'
