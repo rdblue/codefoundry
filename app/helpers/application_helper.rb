a387c88f3d22574083b933c087e3e51a7ae1aef1
Ryan Blue <rdblue@gmail.com>
2010-02-21 14:01:54 +0100

    Added plain and marker text filters.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,18 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def marker_to_html( markup, options = {} )
+    # Marker can't handle windows newlines and does not do sanitization so we
+    # account for that here.
+    Marker.parse( h( markup.gsub(/\r/, '') ) ).to_html( options )
+  rescue NoMethodError
+    # if Marker cannot parse the markup, it will return Nil and a NoMethodError
+    # will be raised for to_html.  Catch this and return default formatting
+    # with the error message hidden.
+    text_to_html( markup ) + "\n<!-- #{Marker.parser.failure_reason} -->"
+  end
+
+  def text_to_html( text, options = {} )
+    # do the simplest thing for now
+    "<pre>text</pre>"
+  end
 end
