09b880fb61795a6812cd87acf9742a36fdd8ddbc
Ryan Blue <rdblue@gmail.com>
2010-06-13 18:51:48 +0200

    Removed git handler config from routes.
    routes.rb shouldn't contain the configuration information that was previously
    needed to initialize the git handler.  This changed the routes.rb file to
    simply call GitHandler.new and SvnHandler.new.  The config information was
    moved into git_handler.rb until I can find a reliable place for it.

--- lib/git_handler.rb
+++ lib/git_handler.rb
@@ -1,4 +1,17 @@
 require 'grack/lib/git_http'
 
-class GitHandler < GitHttp::App
+class GitHandler
+  def initialize( *args )
+    @git_provider = GitHttp::App.new
+    @git_provider.set_config(
+        :project_root => '/home/blue/tmp',
+        :git_path => '/usr/bin/git',
+        :upload_pack => true,
+        :receive_pack => true
+      )
+  end
+
+  def call( env )
+    @git_provider.call( env )
+  end
 end
