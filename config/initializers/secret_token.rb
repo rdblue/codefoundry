d5d421be0bd64a3e77367f44598b1f4bdca86b5b
Adam Lamar <adamonduty@gmail.com>
2010-06-18 20:44:04 +0100

    Upgrading to rails 3.0.0.beta4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '2e203ff701aefec27e7ef984212a0229ad808d70acd9cbd441e71411106ea125aaa07a0e9f429483760961f6ee341095139d11769f95c51cab82c7a901b00d37'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key    => '_codefoundry_session',
-  :secret => 'c36afd6cdc0ff826c8b672e15892c95fea6239aa7d1247e69d1292bcb7464d4688f14d6320242a7000231040d69b5cc70b45b52d42e36b3ab884579c54997df9'
-}
+Rails.application.config.session_store :cookie_store, :key => '_codefoundry_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# Rails.application.config.session_store :active_record_store
