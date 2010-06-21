99a0616d179a521b0d0111778a513bcc9fd9084a
Ryan Blue <rdblue@gmail.com>
2010-06-21 18:54:26 +0200

    Adding current work on getting the SVN proxy to work.  It's ugly right now.

--- lib/svn_handler.rb
+++ lib/svn_handler.rb
@@ -1,12 +1,14 @@
 require 'uri'
 require 'rack/streaming_proxy'
 
+require 'pp'
+
 class SvnHandler
   def initialize( *args )
     # fake a rack app for StreamingProxy; we use it as an app, not middleware
     fake_app = lambda { |r| [404, {}, []] }
     @svn_provider = Rack::StreamingProxy.new( fake_app ) do |req|
-      "http://localhost:3000/" + req.path.sub(/\/svn\//, '')
+      "http://dev" + req.path.sub(/\/svn\//, '')
     end
   end
 
